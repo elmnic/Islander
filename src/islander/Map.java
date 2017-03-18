122d799e9567b94f4a320e7269b30494632c8a10
nickeboii <nicke_elmang@hotmail.com>
2017-03-18 11:19:56 +0100

    Autocorrected

--- src/islander/Map.java
+++ src/islander/Map.java
@@ -18,15 +18,17 @@ public class Map extends JPanel {
 	public static final int PLAYER = 3;
 	public static final int PLACEDBRIDGE = 4;
 	public static final int GOAL = 5;
-	private Color waterColor = new Color(24, 35, 226);//Blue
-	private Color landColor = new Color(12, 210, 16);//Green
-	private Color bridgeColor = new Color(130, 78, 14);//Brown
-	private Color placedBridgeColor = new Color(206, 124, 22);//Lighter brown
-	private Color playerColor = new Color(247, 13, 9);//Red
-	private Color goalColor = new Color(255, 18, 206);//Pink
+	private Color waterColor = new Color(24, 35, 226);// Blue
+	private Color landColor = new Color(12, 210, 16);// Green
+	private Color bridgeColor = new Color(130, 78, 14);// Brown
+	private Color placedBridgeColor = new Color(206, 124, 22);// Lighter brown
+	private Color playerColor = new Color(247, 13, 9);// Red
+	private Color goalColor = new Color(255, 18, 206);// Pink
 	private int gridWidth = 0;
 	private boolean grid = true;
-	private boolean simultaneous = true; //Refine the blocks on the map all at once or one by one and thus influencing the next block
+	private boolean simultaneous = true; // Refine the blocks on the map all at
+											// once, or one by one and thus
+											// influencing the next block
 	private Player player;
 	private Goal goal;
 	private Bridge bridge;
@@ -47,7 +49,7 @@ public class Map extends JPanel {
 	}
 
 	public void buildMap() {
-		//Fills the board with '0' and '1'
+		// Fills the board with '0' and '1'
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
 				if (ratio < Math.random()) {
@@ -58,7 +60,7 @@ public class Map extends JPanel {
 			}
 		}
 		for (int i = 0; i < 10; i++) {
-			//refineMap();
+			// refineMap();
 		}
 		player.setSpawnWidth(6);
 		player.setSpawnHeight(5);
@@ -69,7 +71,7 @@ public class Map extends JPanel {
 	public void refineMap() {
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
-				//Land
+				// Land
 				if (map[i][j] == 1) {
 					if (adjacentLand(i, j, LAND) < 4) {
 						if (simultaneous) {
@@ -78,7 +80,7 @@ public class Map extends JPanel {
 							map[i][j] = WATER;
 						}
 					}
-				} //Water
+				} // Water
 				else {
 					if (adjacentLand(i, j, LAND) >= 4) {
 						if (simultaneous) {
@@ -100,7 +102,8 @@ public class Map extends JPanel {
 	public void updateMap(int z, boolean placeBridge) {
 		int[][] playerXY = player.getPlayerXY();
 
-		//Updates playerXY and paints the player's previous position with playerWasOn
+		// Updates playerXY and paints the player's previous position with
+		// playerWasOn
 		player.move(playerXY[0][0], playerXY[0][1], z, playerWasOn);
 		if (map[playerXY[0][0]][playerXY[0][1]] != PLAYER) {
 			playerWasOn = map[playerXY[0][0]][playerXY[0][1]];
@@ -109,7 +112,8 @@ public class Map extends JPanel {
 			if (placeBridge && player.getPlayerInv() > 0) {
 				placeBridge(playerXY);
 			} else {
-				//Moves the player in the opposite direction if the player tries to go on water and isn't Jesus
+				// Moves the player in the opposite direction if the player
+				// tries to go on water and isn't Jesus
 				switch (z) {
 				case 0:
 					z = 2;
@@ -126,7 +130,8 @@ public class Map extends JPanel {
 				}
 				stayOffWater(playerXY, z, playerWasOn);
 			}
-			//Gets the block the player will be on and uses it to paint it back when the player moves away again
+			// Gets the block the player will be on and uses it to paint it back
+			// when the player moves away again
 		} else {
 			paintNewPlayerPos(playerXY);
 		}
@@ -135,59 +140,68 @@ public class Map extends JPanel {
 		}
 	}
 
-	//Keeps the player off water...
+	// Keeps the player off water...
 	public void stayOffWater(int[][] playerXY, int z, int playerWasyOn) {
-		//Block which the player went FROM into water
+		// Block which the player went FROM into water
 		playerWasOn = playerXY[0][2];
 
-		//Moves the player in the opposite direction
+		// Moves the player in the opposite direction
 		player.move(playerXY[0][0], playerXY[0][1], z, playerWasOn);
 		map[playerXY[0][0]][playerXY[0][1]] = PLAYER;
 	}
 
-	//Places a bridge block in the direction the player last tried to go, if the next block in that direction is water
+	// Places a bridge block in the direction the player last tried to go, if
+	// the next block in that direction is water
 	public void placeBridge(int[][] playerXY) {
 		playerWasOn = PLACEDBRIDGE;
 		player.setPlayerInv(player.getPlayerInv() - 1);
 
-		//Paints the block the player is on
+		// Paints the block the player is on
 		map[playerXY[0][0]][playerXY[0][1]] = PLAYER;
-		map[playerXY[1][0]][playerXY[1][1]] = playerXY[0][2]; //ID of the block that was there before the player
+		map[playerXY[1][0]][playerXY[1][1]] = playerXY[0][2]; // ID of the block
+																// that was
+																// there before
+																// the player
 	}
 
-	//Paints the player's new position and paints it's previous position back to its original state
+	// Paints the player's new position and paints it's previous position back
+	// to its original state
 	public void paintNewPlayerPos(int[][] playerXY) {
-		//Paints the block the player is on
+		// Paints the block the player is on
 		map[playerXY[0][0]][playerXY[0][1]] = PLAYER;
 
-		//Picks up the bridge block and adds it to the player's inventory
+		// Picks up the bridge block and adds it to the player's inventory
 		if (playerWasOn == BRIDGE) {
 			player.pickUpBridge();
 			bridge.setBridgeOnMap(bridge.getBridgeOnMap() - 1);
 		}
 
-		//Paints the block the player WAS on back to its original state
-		map[playerXY[1][0]][playerXY[1][1]] = playerXY[0][2]; //ID of the block that was there before the player
+		// Paints the block the player WAS on back to its original state
+		map[playerXY[1][0]][playerXY[1][1]] = playerXY[0][2]; // ID of the block
+																// that was
+																// there before
+																// the player
 	}
 
-	//Refines the map
+	// Refines the map
 	public int adjacentLand(int x, int y, int search) {
 		int found = 0;
 		for (int i = -1; i <= 1; i++) {
 			for (int j = -1; j <= 1; j++) {
-				if ((x + i >= 0) && (y + j >= 0) && (x + i < map.length) && (y + j < map[0].length) && (map[x + i][y + j] == search)) {
+				if ((x + i >= 0) && (y + j >= 0) && (x + i < map.length) && (y + j < map[0].length)
+						&& (map[x + i][y + j] == search)) {
 					found++;
 				}
 			}
 		}
-		//Subtracts the instance where it finds itself
+		// Subtracts the instance where it finds itself
 		if (found != 0) {
 			found--;
 		}
 		return found;
 	}
 
-	//Adds the player
+	// Adds the player
 	public void addPlayer(boolean newGame) {
 		playerWasOn = LAND;
 		int[][] playerXY = player.getPlayerXY();
@@ -203,7 +217,7 @@ public class Map extends JPanel {
 		}
 	}
 
-	//Adds the initial bridge blocks on the map
+	// Adds the initial bridge blocks on the map
 	public void addBridge() {
 		bridge.createBridge();
 		int[][] bridgeMap = bridge.getBridgeMap();
@@ -240,14 +254,14 @@ public class Map extends JPanel {
 		g.setColor(Color.black);
 		if (!won) {
 			setBackground(Color.blue);
-			//Grid lines
+			// Grid lines
 			if (grid) {
 				gridWidth = 1;
-				//Horizontal lines
+				// Horizontal lines
 				for (int i = 0; i < height + 1; i++) {
 					g.drawLine(0, i * scale, width * scale, i * scale);
 				}
-				//Vertical lines
+				// Vertical lines
 				for (int j = 0; j < width + 1; j++) {
 					g.drawLine(j * scale, 0, j * scale, height * scale);
 				}
@@ -257,36 +271,44 @@ public class Map extends JPanel {
 
 			for (int i = 0; i < map.length; i++) {
 				for (int j = 0; j < map[0].length; j++) {
-					if (map[i][j] == WATER) {//Water color
+					if (map[i][j] == WATER) {// Water color
 						g.setColor(waterColor);
-					} else if (map[i][j] == LAND) {//Land color
+					} else if (map[i][j] == LAND) {// Land color
 						g.setColor(landColor);
-					} else if (map[i][j] == PLAYER) {//Player color, Red and White
+					} else if (map[i][j] == PLAYER) {// Player color, Red and
+														// White
 						if (!Jesus) {
 							g.setColor(playerColor);
 						} else {
 							g.setColor(Color.white);
 						}
-					} else if (map[i][j] == BRIDGE) {//Bridge color
+					} else if (map[i][j] == BRIDGE) {// Bridge color
 						g.setColor(bridgeColor);
-					} else if (map[i][j] == PLACEDBRIDGE) {//Placed bridge color
+					} else if (map[i][j] == PLACEDBRIDGE) {// Placed bridge
+															// color
 						g.setColor(placedBridgeColor);
-					} else if (map[i][j] == GOAL) {//Goal color
+					} else if (map[i][j] == GOAL) {// Goal color
 						g.setColor(goalColor);
 					}
 					g.fillRect(i * scale + gridWidth, j * scale + gridWidth, scale - gridWidth, scale - gridWidth);
 				}
 			}
-			
-			//The player spawn area
+
+			// The player spawn area
 			g.setColor(Color.RED);
-			g.drawLine(width * scale / player.getSpawnWidth() - 3, 0, width * scale / player.getSpawnWidth() - 3, height * scale / player.getSpawnHeight());
-			g.drawLine(0, height * scale / player.getSpawnHeight(), width * scale / player.getSpawnWidth() - 3, height * scale / player.getSpawnHeight());
-			//The goal spawn area
+			g.drawLine(width * scale / player.getSpawnWidth() - 3, 0, width * scale / player.getSpawnWidth() - 3,
+					height * scale / player.getSpawnHeight());
+			g.drawLine(0, height * scale / player.getSpawnHeight(), width * scale / player.getSpawnWidth() - 3,
+					height * scale / player.getSpawnHeight());
+			// The goal spawn area
 			g.setColor(Color.RED);
-			g.drawLine((width - (width / goal.getSpawnWidth())) * scale, (height - (height / goal.getSpawnHeight())) * scale, width * scale, (height - (height / goal.getSpawnHeight())) * scale);
-			g.drawLine((width - (width / goal.getSpawnWidth())) * scale, (height - (height / goal.getSpawnHeight())) * scale, (width - (width / goal.getSpawnWidth())) * scale, height * scale);
-		
+			g.drawLine((width - (width / goal.getSpawnWidth())) * scale,
+					(height - (height / goal.getSpawnHeight())) * scale, width * scale,
+					(height - (height / goal.getSpawnHeight())) * scale);
+			g.drawLine((width - (width / goal.getSpawnWidth())) * scale,
+					(height - (height / goal.getSpawnHeight())) * scale,
+					(width - (width / goal.getSpawnWidth())) * scale, height * scale);
+
 		} else {
 			setBackground(goalColor);
 			String text = "You Won!";
@@ -294,10 +316,10 @@ public class Map extends JPanel {
 			g.setFont(getFont().deriveFont(50.0f));
 			int totalWidth = (fm.stringWidth(text) * 2) + 4;
 
-            // Baseline
-            int x = (getWidth() - totalWidth) / 2;
-            int y = (getHeight() - fm.getHeight()) / 2;
-            g.drawString(text, x, y + ((fm.getDescent() + fm.getAscent()) / 2));
+			// Baseline
+			int x = (getWidth() - totalWidth) / 2;
+			int y = (getHeight() - fm.getHeight()) / 2;
+			g.drawString(text, x, y + ((fm.getDescent() + fm.getAscent()) / 2));
 		}
 	}
 
